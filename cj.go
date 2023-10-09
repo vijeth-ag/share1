f6991528c05c9d8f371b9df80a0d68a36da88719
vijeth.ag <34409546+vijeth-ag@users.noreply.github.com>
2023-10-09 13:29:41 +0530

    Update cj.go

--- cj.go
+++ cj.go
@@ -6,61 +6,59 @@ import (
     "os"
     "time"
 
-    batchv1 "k8s.io/api/batch/v1beta1"
-    "k8s.io/apimachinery/pkg/util/intstr"
     "k8s.io/client-go/kubernetes"
     "k8s.io/client-go/rest"
     "k8s.io/client-go/util/homedir"
     "k8s.io/client-go/util/kubeconfig"
+    "github.com/robfig/cron/v3"
 )
 
 func main() {
+    // Initialize cron scheduler.
+    c := cron.New()
+
+    // Add a cron job to delete the ServiceAccount every 30 minutes.
+    _, err := c.AddFunc("*/30 * * * *", func() {
+        deleteServiceAccount()
+    })
+    if err != nil {
+        fmt.Printf("Error adding cron job: %v\n", err)
+        os.Exit(1)
+    }
+
+    // Start the cron scheduler.
+    c.Start()
+
+    // Run the program indefinitely.
+    select {}
+}
+
+func deleteServiceAccount() {
     // Load Kubernetes configuration from the default location or use the one specified in KUBECONFIG.
     kubeconfigPath := filepath.Join(homedir.HomeDir(), ".kube", "config")
     config, err := kubeconfig.NewForConfig(kubeconfigPath)
     if err != nil {
         fmt.Printf("Error loading kubeconfig: %v\n", err)
-        os.Exit(1)
+        return
     }
 
     // Create a Kubernetes clientset.
     clientset, err := kubernetes.NewForConfig(config)
     if err != nil {
         fmt.Printf("Error creating clientset: %v\n", err)
-        os.Exit(1)
+        return
     }
 
-    // Define the CronJob object.
-    cronJob := &batchv1.CronJob{
-        ObjectMeta: metav1.ObjectMeta{
-            Name: "my-cronjob",
-        },
-        Spec: batchv1.CronJobSpec{
-            Schedule: "*/1 * * * *", // Replace with your desired schedule.
-            JobTemplate: batchv1.JobTemplateSpec{
-                Spec: batchv1.JobSpec{
-                    Template: corev1.PodTemplateSpec{
-                        Spec: corev1.PodSpec{
-                            Containers: []corev1.Container{
-                                {
-                                    Name:  "my-container",
-                                    Image: "my-image:latest", // Replace with your container image.
-                                },
-                            },
-                        },
-                    },
-                },
-            },
-        },
-    }
+    // Define the ServiceAccount name and namespace to delete.
+    namespace := "your-namespace" // Replace with the desired namespace.
+    serviceAccountName := "your-serviceaccount" // Replace with the desired ServiceAccount name.
 
-    // Create the CronJob in the desired namespace.
-    namespace := "default" // Replace with your desired namespace.
-    createdCronJob, err := clientset.BatchV1beta1().CronJobs(namespace).Create(context.TODO(), cronJob, metav1.CreateOptions{})
+    // Delete the ServiceAccount.
+    err = clientset.CoreV1().ServiceAccounts(namespace).Delete(context.TODO(), serviceAccountName, metav1.DeleteOptions{})
     if err != nil {
-        fmt.Printf("Error creating CronJob: %v\n", err)
-        os.Exit(1)
+        fmt.Printf("Error deleting ServiceAccount: %v\n", err)
+        return
     }
 
-    fmt.Printf("CronJob created with name: %s\n", createdCronJob.Name)
+    fmt.Printf("ServiceAccount %s in namespace %s deleted\n", serviceAccountName, namespace)
 }
